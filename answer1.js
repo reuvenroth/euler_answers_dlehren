5abb5ecd33ba2bb0e5e207e909847feb23a8e6c5
dlehren <dlehren@users.noreply.github.com>
2014-11-29 21:52:59 -0500

    i found the first answer

---
+++ answer1.js
@@ -0,0 +1,12 @@
+var add = [];
+for(i = 0; i<1000; i+=5){
+     add.push(i);
+}
+
+
+var add2 = [];
+for(i = 0; i<1000; i+=3){
+    add2.push(i);
+    }
+    var tatal = add.length + add2.length;
+    console.log (tatal);
